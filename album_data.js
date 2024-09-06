4f22fa944fd1b70727086a27ccc7951f51172ffc
SmallCakekoo <payan.sary@gmail.com>
2024-09-06 04:33:54 -0500

    Primer Commit

---
+++ album_data.js
@@ -0,0 +1,25 @@
+// database de albumes
+const albumData = {
+    "1.jpg": { title: "Dark & Wild", date: "19 / 08 / 2014", price: "$20.74" },
+    "2.jpg": { title: "The Most Beautiful Moment in Life PT1", date: "29 / 04 / 2015", price: "$17.24" },
+    "3.jpg": { title: "The Most Beautiful Moment in Life PT2", date: "30 / 11 / 2015", price: "$23.99" },
+    "4.jpg": { title: "O!RUL8,2?", date: "11 / 09 / 2013", price: "$13.99" },
+    "5.jpg": { title: "Wings", date: "10 / 10 / 2016", price: "$25.97" },
+    "6.jpg": { title: "You Never Walk Alone", date: "13 / 02 / 2017", price: "$34.90" },
+    "7.jpg": { title: "Love Yourself 承 'Her'", date: "18 / 09 / 2017", price: "$23.49" },
+    "8.jpg": { title: "Love Yourself 轉 'Tear'", date: "18 / 05 / 2018", price: "$19.25" },
+    "9.jpg": { title: "Love Yourself 結 'Answer'", date: "24 / 08 / 2018", price: "$24.99" },
+    "10.jpg": { title: "Map of the Soul: Persona", date: "12 / 04 / 2019", price: "$19.05" },
+    "11.jpg": { title: "TMBMIL: Young Forever", date: "2 / 05 / 2016", price: "$28.99" },
+    "12.jpg": { title: "2 Cool 4 Skool", date: "13 / 06 / 2013", price: "$13.00" },
+    "13.jpg": { title: "Skool Luv Affair", date: "12 / 02 / 2014", price: "$17.93" },
+    "14.jpg": { title: "Butter", date: "9 / 07 / 2021", price: "$20.74" },
+    "15.jpg": { title: "Butter Digital Single", date: "21 / 05 / 2021", price: "$23.99" },
+    "16.png": { title: "Take Two", date: "8 / 06 / 2023", price: "$1.29" },
+    "17.png": { title: "Proof", date: "10 / 06 / 2022", price: "$17.92" },
+    "18.jpg": { title: "Map of the Soul: 7", date: "21 / 02 / 2020", price: "$23.96" },
+    "19.jpg": { title: "DYNAMITE", date: "21 / 08 / 2020", price: "$13.74" },
+    "20.jpg": { title: "Be", date: "20 / 11 / 2020", price: "$34.99" }
+};
+
+export default albumData;
